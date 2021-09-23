34d6a40d62524ff05a3305ad2d07a7b9af85c9fd
Karuitha <diakingathia2005@gmail.com>
2021-09-23 21:02:58 +0200

    Updated write up

--- 100metres/code/sprints.R
+++ 100metres/code/sprints.R
@@ -6,6 +6,7 @@ library(lubridate)
 library(skimr)
 library(countrycode)
 
+## @knitr webscrap
 pages <- 1:224
 
 
@@ -34,6 +35,9 @@ scrapper <- function(x){
 # my_100_dash_data <- pages %>% map_dfr(~ scrapper(.x))
 
 # write_csv(my_100_dash_data, "my_100_dash_data.csv")
+
+
+## @knitr data_cleaning
 
 my_100_dash_data <- read_csv("data/my_100_dash_data.csv") %>% 
         
@@ -77,23 +81,75 @@ my_100_dash_data <- read_csv("data/my_100_dash_data.csv") %>%
                                               
                                               ))
 
-## Check out NAs
+## @knitr nas
 
-subset(my_100_dash_data, subset = is.na(dob))
+Amelia::missmap(my_100_dash_data)
 
-my_100_dash_data[is.na(my_100_dash_data$dob), ]  
+sapply(my_100_dash_data, is.na) %>% 
+        
+        colSums() %>% 
+        
+        tibble(variables = names(my_100_dash_data), missing = .) %>% 
+        
+        arrange(desc(missing)) %>% 
+        
+        mutate(prop_percent = missing / nrow(my_100_dash_data) * 100) %>% 
+        
+        head(10)
 
-## athletes by country
+
+## @knitr most_athletes_country
+
 my_100_dash_data %>% 
         
-        count(nat, sort = TRUE)
-
-
-## Check out age structure of the fastest athletes. 
+        count(nat, sort = TRUE) 
 
 my_100_dash_data %>% 
         
-        skim(age_years)
+        select(competitor, nat) %>% 
+        
+        filter(!duplicated(.)) %>% 
+        
+        count(nat) %>% 
+        
+        arrange(desc(n))
+
+## @knitr top_athletes_best_times
+my_100_dash_data %>% 
+        
+        select(competitor, mark) %>% 
+        
+        arrange(mark) %>% 
+        
+        head(10)
+
+## @knitr top_all_time_best
+my_100_dash_data %>% 
+        
+        select(competitor, mark) %>% 
+        
+        arrange(mark) %>% 
+        
+        select(competitor) %>% 
+        
+        filter(!duplicated(.)) %>% 
+        
+        head(10)
+
+## @knitr top_athlete_appearances
+
+my_100_dash_data %>% 
+        
+        count(competitor, sort = TRUE) %>% 
+        
+        head(10)
+
+
+## @knitr age_structure_times
+
+my_100_dash_data %>% 
+        
+        skim_without_charts(age_years, mark)
 
 ## Youngest athlete 
 my_100_dash_data[which.min(my_100_dash_data$age_years), ]
@@ -101,6 +157,8 @@ my_100_dash_data[which.min(my_100_dash_data$age_years), ]
 ## Oldest athlete 
 my_100_dash_data[which.max(my_100_dash_data$age_years), ]
 
+
+## @knitr data_vis
 ## Graph of the age structure
 
 my_100_dash_data %>% 
